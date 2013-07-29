f6bac0291c5b34389a5fa2e605b41dc5157f51a3
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-07-29 09:29:02 -0700

    Established mentor match app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentorMatch::Application.config.secret_token = '029d6df0bc82db6410ca1076eaec92a57062cb812dca173cc06edf02bab03e8340862e3015bed221611d3957a27bb4691d5a866c9ffd1709a9f24d2adf501065'
