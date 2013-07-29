8cfbfc4a5f7028b94b301c853dd7049a8457bcfe
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-07-29 09:52:38 -0700

    Fixed typo in mentor model

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,6 +1,6 @@
 class Mentor < ActiveRecord::Base
   has_many :mentorships
-  has_many :mentees, :through => mentorships
+  has_many :mentees, :through => :mentorships
 
   attr_accessible :affiliation, :email, :name
 
